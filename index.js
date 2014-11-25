87ecaf655c3de5b21c4aca81aa76597a5dd62d4e
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2014-11-25 14:40:09 +0100

    Simplify code structure

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = require('./lib/index');
+/*
+** Module dependencies
+*/
+var Client = require('./lib/client');
+
+
+/*
+** Methods
+*/
+function csw(url, options) {
+    return new Client(url, options);
+}
+
+
+/*
+** Exports
+*/
+module.exports = csw;
